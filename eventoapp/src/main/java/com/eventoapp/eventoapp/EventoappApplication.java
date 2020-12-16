7063f8ea498fb5a176cea871ec1615757a6bb13d
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2020-12-16 01:53:31 -0400

    Criacao do controller e da view de evento

--- eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -1,9 +1,12 @@
 package com.eventoapp.eventoapp;
 
+import com.eventoapp.eventoapp.controllers.EventoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses= EventoController.class)
 public class EventoappApplication {
 
 	public static void main(String[] args) {
