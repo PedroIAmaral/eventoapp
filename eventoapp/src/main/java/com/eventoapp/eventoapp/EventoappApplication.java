2f5ab2b718aeb19b55f7dc7fdf3845ff26270afd
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2020-12-17 22:36:57 -0400

    Configuracao de banco de dados e criacao de formulario de insercao

--- eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -1,12 +1,15 @@
 package com.eventoapp.eventoapp;
 
 import com.eventoapp.eventoapp.controllers.EventoController;
+import com.eventosapp.eventoapp.repository.EventoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses= EventoController.class)
+@EnableJpaRepositories(basePackageClasses = EventoRepository.class)
 public class EventoappApplication {
 
 	public static void main(String[] args) {
